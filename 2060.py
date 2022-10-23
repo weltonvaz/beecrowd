2fea6baa522baa8b87333cf53a4a7d41a78dd4c7
Welton Vaz <weltonvaz@gmail.com>
2022-10-22 22:47:17 -0300

    adicionando desafio: 2060 EM Python

--- 2060.py
+++ 2060.py
@@ -11,11 +11,26 @@ A primeira linha da entrada consiste em um inteiro N (1 ≤ N ≤1000), represen
 A segunda linha contém N inteiros Li (1 ≤ Li ≤ 100), representando os números da lista de Bino.
 
 Saída
-Imprima a quantidade de números múltiplos de 2, 3, 4 e 5 presentes na lista. Observe a formatação da saída nos exemplos, pois ela deve ser seguida rigorosamente.
+Imprima a quantidade de números múltiplos de 2, 3, 4 e 5 presentes na lista. Observe a formatação da saída nos exemplos,
+pois ela deve ser seguida rigorosamente.
 '''
-from sympy import divisor_count
 
 n = int(input())
 wert = [int(x) for x in input().split()]
 
-print(wert)
+count2, count3, count4, count5 = 0, 0, 0, 0
+
+for x in wert:
+    if x % 2 == 0:
+        count2 += 1
+    if x % 3 == 0:
+        count3 += 1
+    if x % 4 == 0:
+        count4 += 1
+    if x % 5 == 0:
+        count5 += 1
+
+print(f"{count2} Multiplo(s) de 2")
+print(f"{count3} Multiplo(s) de 3")
+print(f"{count4} Multiplo(s) de 4")
+print(f"{count5} Multiplo(s) de 5")
---
+++ 2061.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+Sua tarefa é descobrir com quantas abas que o navegador de Péricles ficou, sabendo o número inicial de abas e a sequência de ações de Péricles. As ações podem ser fechou (quando Péricles fechou uma aba) ou clicou (quando Péricles clicou em uma propaganda).
+
+Entrada
+A entrada é iniciada por uma linha contendo dois números inteiros positivos, N e M (0 < N, M < 500), representando o número inicial de abas e o número de ações de Péricles. Cada linha subsequente contém uma ação (fechou ou clicou). Naturalmente, o número de abas é sempre maior ou igual a zero.
+
+Saída
+A saída deve ser uma linha contendo o número final de abas.
+'''
